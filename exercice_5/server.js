5087571645875d10f4cd95a65e65eaaa09e64995
Mars Hamza <hamza31mars@gmail.com>
2025-10-16 15:56:37 +0200

    exo5

--- exercice_5/server.js
+++ exercice_5/server.js
@@ -3,21 +3,24 @@ const app = express();
 const path = require('path');
 
 function loggerMiddleware(req, res, next) {
-  console.log('nouvelle requête entrante');
+  console.log('nouvelle requête entrante, req.body =', req.body);
   next();
 }
+
+
 
 app.use(express.json());
 app.use(loggerMiddleware);
 
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'templates')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/test', (req, res) => {
   res.json({ body: req.body });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
 app.listen(3000, () => console.log('Serveur Express démarré sur http://localhost:3000'));
